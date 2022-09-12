1b5ff38baad29424bfa45c5647434b49fb436de8
Omurbek <omke_aka@mail.ru>
2022-09-12 11:22:08 +0600

    attemp

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+    boolean god = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+
+    if (god){
+        System.out.println("leap");
+    }else {
+        System.out.println("not leap");
+    }
     }
 }
